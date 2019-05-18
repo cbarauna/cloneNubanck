36881d465e7c1ce6b60622d3994c41416e042378
Carlos Barauna <cbarauna@MacBook-Air-de-Carlos.local>
2019-05-18 00:16:51 -0300

    Estrutura Nubank RocketSeat

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,43 @@
 import React from 'react';
-import { Container } from './style';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './style';
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="atach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo Disponivel</Title>
+            <Description>R$ 197.611,00</Description>
+          </CardContent>
+
+          <CardFooter>
+            <Annotation>Transferencia de R$ 10.000,00 recebida</Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
+      <Tabs />
+    </Container>
+  );
 }
